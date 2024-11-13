6418aec74108cbac48325fa34c57ef38f2b23cc2
Oscar <119875972+Oscarrain@users.noreply.github.com>
2024-11-13 21:23:01 +0800

    usercenter debug

--- frontend/services/usercenter/fetchUsercenter.js
+++ frontend/services/usercenter/fetchUsercenter.js
@@ -1,9 +1,15 @@
-import { config } from '../../config/index';
+import {
+  config
+} from '../../config/index';
 
 /** 获取个人中心信息 */
 function mockfetchUserData() {
-  const { delay } = require('../_utils/delay');
-  const { genUsercenter } = require('../../model/usercenter');
+  const {
+    delay
+  } = require('../_utils/delay');
+  const {
+    genUsercenter
+  } = require('../../model/usercenter');
   return delay(200).then(() => genUsercenter());
 }
 
@@ -12,9 +18,8 @@ export function fetchUserData() {
   return new Promise((resolve, reject) => {
     // 模拟获取用户数据
     resolve({
-      avatar: './avatar.jpeg',
+      avatar: 'https://cloud.tsinghua.edu.cn/f/9d512f8861a842f38d48/?dl=1',
       name: 'User Anonymous',
     });
   });
 }
-
