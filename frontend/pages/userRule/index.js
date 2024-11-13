40d02110999b00ee6a3ee127c1c35fb4c18ff6bb
Oscar <119875972+Oscarrain@users.noreply.github.com>
2024-11-14 00:04:04 +0800

    settings and useracknowledgement and debug

---
+++ frontend/pages/userRule/index.js
@@ -0,0 +1,71 @@
+// pages/userRule/index.js
+Page({
+  onAgree: function () {
+    wx.navigateBack({
+      delta: 1 // 返回上一页
+    });
+  },
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  }
+})
