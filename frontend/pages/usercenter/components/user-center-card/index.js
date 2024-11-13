75d322fe60f9811e8dc3ee6b902620cc19e68255
Oscar <119875972+Oscarrain@users.noreply.github.com>
2024-11-13 19:29:41 +0800

    user center front viewable now

--- frontend/pages/usercenter/components/user-center-card/index.js
+++ frontend/pages/usercenter/components/user-center-card/index.js
@@ -1,35 +1,11 @@
-const AuthStepType = {
-  ONE: 1,
-  TWO: 2,
-  THREE: 3,
-};
-
+// usercenter/user-center-card/index.js
 Component({
-  options: {
-    multipleSlots: true,
-  },
   properties: {
-    currAuthStep: {
-      type: Number,
-      value: AuthStepType.ONE,
-    },
-    userInfo: {
-      type: Object,
-      value: {},
-    },
-    isNeedGetUserInfo: {
-      type: Boolean,
-      value: false,
-    },
-  },
-  data: {
-    defaultAvatarUrl:
-      'https://cdn-we-retail.ym.tencent.com/miniapp/usercenter/icon-user-center-avatar@2x.png',
-    AuthStepType,
-  },
-  methods: {
-    gotoUserEditPage() {
-      this.triggerEvent('gotoUserEditPage');
-    },
+    avatar: String,
+    name: String,
+    likes: Number,
+    comments: Number,
+    viewed: Number,
+    liked: Number,
   },
 });
--- frontend/services/usercenter/fetchUsercenter.js
+++ frontend/services/usercenter/fetchUsercenter.js
@@ -1,18 +1,20 @@
 import { config } from '../../config/index';
 
 /** 获取个人中心信息 */
-function mockFetchUserCenter() {
+function mockfetchUserData() {
   const { delay } = require('../_utils/delay');
   const { genUsercenter } = require('../../model/usercenter');
   return delay(200).then(() => genUsercenter());
 }
 
 /** 获取个人中心信息 */
-export function fetchUserCenter() {
-  if (config.useMock) {
-    return mockFetchUserCenter();
-  }
-  return new Promise((resolve) => {
-    resolve('real api');
+export function fetchUserData() {
+  return new Promise((resolve, reject) => {
+    // 模拟获取用户数据
+    resolve({
+      avatar: 'https://example.com/avatar.jpg',
+      name: 'User Anonymous',
+    });
   });
 }
+
