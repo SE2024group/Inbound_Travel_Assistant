73914d6e690da53448a4c1290b22cecf3a7490e0
zrz <zhangrz22@mails.tsinghua.edu.cn>
2024-11-22 00:30:45 +0800

    feat(out):click the downloadlink to jump to a web

--- frontend/pages/appDetail/appDetail.js
+++ frontend/pages/appDetail/appDetail.js
@@ -7,14 +7,14 @@ Page({
         title: 'Gaode Map',
         image: 'https://cloud.tsinghua.edu.cn/f/024c76f8b1234965b23b/?dl=1',
         label: ['detailed map','prcise navigation'],
-        download_link: 'www.gaode.com',
+        download_link: 'https://www.baidu.com',
         description:'Gaode Map (Amap) is a popular Chinese mapping and navigation application developed by Alibaba Group. It provides users with detailed maps, real-time traffic updates, and precise navigation features for driving, walking, and public transportation. With a wide range of functionalities, Gaode Map is known for its user-friendly interface and accurate location services. It offers features such as voice-guided navigation, 3D map views, route planning, and points of interest (POI) search, making it an essential tool for everyday travel in China. Additionally, it supports offline maps, which is particularly useful in areas with poor internet connectivity.'
       },
       huazhuhui: {
         title: 'Huazhu Club',
         image: 'https://cloud.tsinghua.edu.cn/f/d56fe614d79c4d7a855c/?dl=1',
         label: ['hotel booking','budget-friendly'],
-        download_link: 'www.huazhuhui.com',
+        download_link: 'www.google.com',
         description: 'The Huazhu Club app, is a comprehensive hotel booking platform primarily designed for Chinese travelers. The app offers access to over a thousand hotels under the Huazhu brand, including well-known chains like Hanting, Joya, and Citadines. Users can browse hotel options, make reservations, and manage their bookings all in one place. The app provides a loyalty program, allowing users to earn points on bookings that can be redeemed for discounts, upgrades, and exclusive member benefits. Additionally, it includes features such as contactless check-in and checkout, personalized travel recommendations, and special deals. With a user-friendly interface and a range of hotel options from budget-friendly to luxury, the Huazhu Club app has become an essential tool for millions of travelers in China.'
       },
       xiecheng: {
@@ -69,6 +69,23 @@ Page({
     this.setData({
       id: options.id
     });
+  },
+
+  
+  toOut(event) {
+    const { id, items } = this.data;
+    const url = items[id].download_link;
+
+    if (url) {
+      wx.navigateTo({
+        url: `/pages/out/out?url=${encodeURIComponent(url)}`
+      });
+    } else {
+      wx.showToast({
+        title: '下载链接不存在',
+        icon: 'none'
+      });
+    }
   },
 
   /**
