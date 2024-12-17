72bf8c7c486e3770128f0e0973f86e519ce54e23
Oscar <119875972+Oscarrain@users.noreply.github.com>
2024-12-17 19:29:20 +0800

    update(myLikes): Mylikes functions primalily realized

--- frontend/pages/myLikes/index.js
+++ frontend/pages/myLikes/index.js
@@ -1,19 +1,89 @@
 // pages/myLikes/index.js
-Page({
 
+import {
+  fetchFavoriteGoodsList
+} from '../../services/good/fetchGoods'; // 假设有一个获取收藏列表的服务接口
+import Toast from 'tdesign-miniprogram/toast/index';
+
+Page({
   /**
    * Page initial data
    */
   data: {
-
+    goodsList: [],
+    goodsListLoadStatus: 0, // 0: 可继续加载, 1: 加载中, 3: 加载失败
+    pageLoading: false,
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad(options) {
-
+  onLoad() {
+    this.init();
   },
+
+  onPullDownRefresh() {
+    this.init();
+  },
+
+  // 初始化页面数据
+  init() {
+    wx.stopPullDownRefresh();
+    this.setData({
+      pageLoading: true
+    });
+    this.loadFavoriteGoodsList(true);
+  },
+
+  // 加载用户收藏的菜品列表，fresh为true表示重新加载数据
+  async loadFavoriteGoodsList(fresh = false) {
+    if (fresh) {
+      wx.pageScrollTo({
+        scrollTop: 0,
+      });
+    }
+
+    this.setData({
+      goodsListLoadStatus: 1
+    });
+
+    try {
+      const nextList = await fetchFavoriteGoodsList();
+      // 假设fetchFavoriteGoodsList()返回当前用户收藏的所有商品，不分页
+      this.setData({
+        goodsList: nextList,
+        goodsListLoadStatus: 0,
+        pageLoading: false,
+      });
+    } catch (err) {
+      this.setData({
+        goodsListLoadStatus: 3,
+        pageLoading: false,
+      });
+    }
+  },
+
+  onReTry() {
+    this.loadFavoriteGoodsList();
+  },
+
+  goodListClickHandle(e) {
+    const {
+      index
+    } = e.detail;
+    const {
+      spuId
+    } = this.data.goodsList[index];
+    wx.navigateTo({
+      url: `/pages/goods/details/index?spuId=${spuId}`,
+    });
+  },
+
+  goodListAddCartHandle() {
+    Toast({
+      context: this,
+      selector: '#t-toast',
+      message: '已收藏',
+    });
+  },
+
 
   /**
    * Lifecycle function--Called when page is initially rendered
@@ -40,13 +110,6 @@ Page({
    * Lifecycle function--Called when page unload
    */
   onUnload() {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh() {
 
   },
 
