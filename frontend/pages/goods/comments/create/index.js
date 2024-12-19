f23c0f15067bfa6c8d699a673dfe105d46c0bde0
AuroraSolitude <zhangts22@mails.tsinghua.edu.cn>
2024-12-19 12:05:16 +0800

    basic ok, one file at a time

--- frontend/pages/goods/comments/create/index.js
+++ frontend/pages/goods/comments/create/index.js
@@ -30,29 +30,42 @@ Page({
   },
 
   onRateChange(e) {
-    const { value } = e?.detail;
+    const {
+      value
+    } = e?.detail;
     const item = e?.currentTarget?.dataset?.item;
-    this.setData({ [item]: value }, () => {
+    this.setData({
+      [item]: value
+    }, () => {
       this.updateButtonStatus();
     });
   },
 
   onAnonymousChange(e) {
     const status = !!e?.detail?.checked;
-    this.setData({ isAnonymous: status });
+    this.setData({
+      isAnonymous: status
+    });
   },
 
   handleSuccess(e) {
-    const { files } = e.detail;
-
+    const {
+      files
+    } = e.detail;
+    console.log("Uploaded files:", files); // 打印文件信息，确认是否有 path 属性
     this.setData({
       uploadFiles: files,
     });
   },
 
   handleRemove(e) {
-    const { index } = e.detail;
-    const { uploadFiles } = this.data;
+    const {
+      index
+    } = e.detail;
+
+    const {
+      uploadFiles
+    } = this.data;
     uploadFiles.splice(index, 1);
     this.setData({
       uploadFiles,
@@ -66,21 +79,83 @@ Page({
   },
 
   updateButtonStatus() {
-    const { serviceRateValue, goodRateValue, conveyRateValue, isAllowedSubmit } = this.data;
-    const { textAreaValue } = this;
+    const {
+      serviceRateValue,
+      goodRateValue,
+      conveyRateValue,
+      isAllowedSubmit
+    } = this.data;
+    const {
+      textAreaValue
+    } = this;
     const temp = serviceRateValue && goodRateValue && conveyRateValue && textAreaValue;
-    if (temp !== isAllowedSubmit) this.setData({ isAllowedSubmit: temp });
+    if (temp !== isAllowedSubmit) this.setData({
+      isAllowedSubmit: temp
+    });
   },
 
   onSubmitBtnClick() {
-    const { isAllowedSubmit } = this.data;
+    const {
+      isAllowedSubmit,
+      uploadFiles
+    } = this.data;
     if (!isAllowedSubmit) return;
+
     Toast({
       context: this,
       selector: '#t-toast',
-      message: '评价提交成功',
+      message: 'Successfully posted your comment',
       icon: 'check-circle',
     });
+
     wx.navigateBack();
-  },
+
+    // 检查是否有上传文件
+    if (uploadFiles.length > 0) {
+      uploadFiles.forEach(file => {
+        wx.uploadFile({
+          url: 'http://1.15.174.177/api/comments/upload/',
+          filePath: file.url, // 确保这里传递的是本地文件路径
+          name: 'images', // 表单名，需和API端一致
+          header: {
+            'Authorization': 'Token 9c05df89dbc2e281c74827c35a968a98049b1163',
+          },
+          formData: {
+            'dish': 1,
+            'comment': this.textAreaValue,
+            'rating': this.data.goodRateValue,
+          },
+          success: function (uploadRes) {
+            console.log('上传成功', uploadRes);
+            // 处理上传成功后的响应
+          },
+          fail: function (uploadErr) {
+            console.log('上传失败', uploadErr);
+            // 处理上传失败
+          }
+        });
+      });
+    } else {
+      // 如果没有文件，则只上传评论数据
+      wx.request({
+        url: 'http://1.15.174.177/api/comments/upload/',
+        method: 'POST',
+        header: {
+          'Authorization': 'Token 9c05df89dbc2e281c74827c35a968a98049b1163',
+        },
+        data: {
+          'dish': 1,
+          'comment': this.textAreaValue,
+          'rating': this.data.goodRateValue,
+        },
+        success: function (res) {
+          console.log('评论上传成功', res);
+        },
+        fail: function (err) {
+          console.log('评论上传失败', err);
+        }
+      });
+    }
+  }
+
 });
